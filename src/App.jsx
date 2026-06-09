ca614ff78c2905636050973c8ebc4c6385efc745
Hpsjadoun <jadounhimanshu14@gmail.com>
2026-06-09 09:59:04 +0530

    fix contact import

--- src/App.jsx
+++ src/App.jsx
@@ -271,7 +271,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Experience from "./components/Experience";
 import Certifications from "./components/Certifications";
-import Contact from "./components/Contact";
+import Contact from './components/contact';
 import ResumeTemplate from "./components/ResumeTemplate";
 
 export default function App() {
